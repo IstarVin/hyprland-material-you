d6bac04462f91139efbf2906c66c1d22b6a12f66
IstarVin <ajdeleverio20@gmail.com>
2024-10-31 19:12:47 +0800

    update

--- ags/services/battery_warning.ts
+++ ags/services/battery_warning.ts
@@ -3,15 +3,9 @@
 const battery = await Service.import("battery");
 
 const battery_messages = {
-<<<<<<< HEAD
-    warnLevels: [15, 5],
-    warnTitles: ["Low battery", "Very low battery"],
-    warnMessages: ["Plug in the charger", "You there?"],
-=======
     warnLevels: [15, 5, 1],
     warnTitles: ["Low battery", "Very low battery", "Critical Battery"],
     warnMessages: ["Plug in the charger", "Please plug in the charger", "Your PC is about to shut down"]
->>>>>>> 206781c0dfe78404143c6395af4cbbc056d101c2
 };
 
 let last_warning = 101;
@@ -31,21 +25,6 @@ async function battery_notification() {
         last_warning = 101;
         return;
     }
-<<<<<<< HEAD
-    for (let i = battery_messages.warnLevels.length - 1; i >= 0; i--) {
-        if (
-            percent <= battery_messages.warnLevels[i] && !charging &&
-            !battery_warned
-        ) {
-            battery_warned = true;
-            Utils.execAsync([
-                "bash",
-                "-c",
-                `notify-send "${battery_messages.warnTitles[i]}" "${battery_messages.warnMessages[i]}" -u critical &`,
-            ]).catch(print);
-            break;
-        }
-=======
 
     const warningLevel = battery_messages.warnLevels
         .slice()
@@ -56,7 +35,6 @@ async function battery_notification() {
         const index = battery_messages.warnLevels.indexOf(warningLevel);
         last_warning = percent;
         await notifyUser(index);
->>>>>>> 206781c0dfe78404143c6395af4cbbc056d101c2
     }
 }
 
