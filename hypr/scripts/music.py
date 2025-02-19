65a2359dc507e1041a8b9fb215449760c06faa62
IstarVin <ajdeleverio20@gmail.com>
2025-02-19 11:18:46 +0800

    update

--- hypr/scripts/music.py
+++ hypr/scripts/music.py
@@ -1,25 +1,17 @@
 #!/bin/python3
 
-
-import json
 import os
 import subprocess
 
-clients_json = subprocess.run(
-    ["hyprctl", "-j", "clients"], capture_output=True, text=True
-).stdout
-clients = json.loads(clients_json)
-
-music_is_open = False
-
-
-for i in clients:
-    print(i.get("title"))
-    if "YouTube Music" in i.get("title"):
-        music_is_open = True
-
+result = subprocess.run(
+    "ps a | grep youtube-music | grep electron",
+    shell=True,
+    capture_output=True,
+    text=True,
+)
+music_is_open = len(result.stdout.strip().split("\n")) > 1
 
 if not music_is_open:
-    os.system("youtube-music")
+    os.system('hyprctl dispatch exec "[workspace special:music silent]" youtube-music')
 
 os.system("playerctl play-pause")
